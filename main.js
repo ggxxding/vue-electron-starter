ab6344d50c7ffc19585de522e2efaf8d8d7f0db6
DMJ <ggxxding@163.com>
2025-06-06 14:54:58 +0800

    add electron

---
+++ main.js
@@ -0,0 +1,28 @@
+const { app, BrowserWindow } = require('electron/main')
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 1920,
+    height: 1080,
+  })
+
+  win.loadFile('./dist/index.html')
+  // devtools
+  win.webContents.openDevTools()
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
